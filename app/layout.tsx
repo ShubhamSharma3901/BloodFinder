b57c299030a7ca985232de0ba2dce6713340a8f9
Shubham Sharma <shubham03901@gmail.com>
2023-12-13 02:27:04 +0530

    Speed Insights added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-import Script from "next/script";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Toaster />
+        <SpeedInsights />
       </body>
     </html>
   );
